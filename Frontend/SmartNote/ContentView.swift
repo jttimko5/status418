5af1bf450fe2f0c78d44861c866dcae386083697
tstauder <55719290+tstauder@users.noreply.github.com>
2023-03-14 19:57:08 -0400

    API test commented out

--- Frontend/SmartNote/ContentView.swift
+++ Frontend/SmartNote/ContentView.swift
@@ -13,10 +13,10 @@ struct ContentView: View {
     @State private var selectedImage: UIImage?
     @State private var isImagePickerDisplay = false
     
-//    API TEST STUFF
-    @State var message = "Some short sample text."
-    private let serverUrl = "https://18.206.89.117/"
-//    END API TEST STUFF
+//    API TEST STUFF      =====================================================
+//    @State var message = "Some short sample text."
+//    private let serverUrl = "http://127.0.0.1:3000"
+//    END API TEST STUFF  =====================================================
     
     var body: some View {
         NavigationView {
@@ -33,46 +33,49 @@ struct ContentView: View {
         }
         
         
-//        TESTING THE API
-        HStack(alignment: VerticalAlignment.top) {
-            Text(verbatim: "TEST API ENDPOINT")
-            TextEditor(text: $message)
-                .padding(EdgeInsets(top: 10, leading: 18, bottom: 0, trailing: 4))
-            Button(action: {
-                let jsonObj = ["text": message]
-                guard let jsonData = try? JSONSerialization.data(withJSONObject: jsonObj) else {
-                    print("contentView: jsonData serialization error")
-                    return
-                }
-                guard let apiUrl = URL(string: serverUrl+"keywords/") else {
-                    print("postChatt: Bad URL")
-                    return
-                }
-                var request = URLRequest(url: apiUrl)
-                request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type") // request is in JSON
-                request.httpMethod = "POST"
-                request.httpBody = jsonData
-                URLSession.shared.dataTask(with: request) { data, response, error in
-                    guard let _ = data, error == nil else {
-                        print("contentView: NETWORKING ERROR")
-                        return
-                    }
-                    if let httpStatus = response as? HTTPURLResponse {
-                        if httpStatus.statusCode != 200 {
-                            print("contentView: HTTP STATUS: \(httpStatus.statusCode)")
-                            return
-                        }
-                    }
-                }.resume()
-                
-            }) {
-                Image(systemName: "paperplane")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 25)
-            }
-        }
-//        END TESTING API
+//        TESTING THE API =====================================================
+//        HStack(alignment: VerticalAlignment.top) {
+//            Text(verbatim: "TEST API ENDPOINT")
+//            TextEditor(text: $message)
+//                .padding(EdgeInsets(top: 10, leading: 18, bottom: 0, trailing: 4))
+//            Button(action: {
+//                let jsonObj = ["text": message]
+//                print(jsonObj)
+//                guard let jsonData = try? JSONSerialization.data(withJSONObject: jsonObj) else {
+//                    print("contentView: jsonData serialization error")
+//                    return
+//                }
+//                guard let apiUrl = URL(string: serverUrl+"keywords") else {
+//                    print("postChatt: Bad URL")
+//                    return
+//                }
+//                var request = URLRequest(url: apiUrl)
+//                request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type") // request is in JSON
+//                request.httpMethod = "POST"
+//                request.httpBody = jsonData
+//                URLSession.shared.dataTask(with: request) { data, response, error in
+//                    guard let _ = data, error == nil else {
+//                        print("contentView: NETWORKING ERROR")
+//                        return
+//                    }
+//                    if let httpStatus = response as? HTTPURLResponse {
+//                        if httpStatus.statusCode != 200 {
+//                            print("contentView: HTTP STATUS: \(httpStatus.statusCode)")
+//                            return
+//                        } else {
+//                            print("Success")
+//                        }
+//                    }
+//                }.resume()
+//                
+//            }) {
+//                Image(systemName: "paperplane")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: 100, height: 25)
+//            }
+//        }
+//        END TESTING API =====================================================
         
         
         HStack (alignment: .bottom){
