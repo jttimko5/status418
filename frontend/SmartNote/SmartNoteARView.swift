4d08bdce354a7b88b8a7f053c5e729ecad4bac94
Kane Sweet <kasweet@umich.edu>
2023-03-16 15:42:03 -0400

    show multiple images in AR with grid arrangement

--- frontend/SmartNote/SmartNoteARView.swift
+++ frontend/SmartNote/SmartNoteARView.swift
@@ -5,11 +5,24 @@
 //  Created by Kane Sweet on 3/16/23.
 //
 
+import Foundation
 import SwiftUI
 import RealityKit
 
 struct SmartNoteARView: View {
-    @StateObject private var viewModel = SmartNoteARViewModel()
+    @StateObject private var viewModel = SmartNoteARViewModel(
+        urls:
+        [
+            // Random test URLs
+            "https://www.applesfromny.com/wp-content/uploads/2020/06/SnapdragonNEW.png",
+            "https://www.applesfromny.com/wp-content/uploads/2020/08/Evercrisp_NYAS-Apples2.png",
+            "https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg",
+            "https://www.applesfromny.com/wp-content/uploads/2020/05/20Ounce_NYAS-Apples2.png",
+            "https://www.applesfromny.com/wp-content/uploads/2020/08/Evercrisp_NYAS-Apples2.png",
+            "https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg",
+            "https://www.applesfromny.com/wp-content/uploads/2020/05/20Ounce_NYAS-Apples2.png"
+        ]
+    )
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
@@ -30,14 +43,13 @@ struct SmartNoteARView: View {
 class SmartNoteARViewModel: ObservableObject {
     let arView = ARView(frame: .zero)
     
+    // Default constructor to test single image from Assets folder
     init() {
-
         // Create a box mesh
-        let mesh = MeshResource.generateBox(size: [0.2, 0.2, 0.001])
-        
-        let imageName = "amongus"
+        let mesh = MeshResource.generateBox(size: [0.2, 0.0001, 0.2])
         
         // Create a material
+        let imageName = "amongus"
         var material = SimpleMaterial()
         material.color.texture = .init(try! .load(named: imageName))
         
@@ -45,9 +57,49 @@ class SmartNoteARViewModel: ObservableObject {
         let entity = ModelEntity(mesh: mesh, materials: [material])
         
         // Create an anchor and add the entity to the scene
-        let anchor = AnchorEntity(world: [0, 0, -0.5])
+        let anchor = AnchorEntity(world: [0, -0.5, -0.5])
         anchor.addChild(entity)
         arView.scene.addAnchor(anchor)
+    }
+    
+    // Constructor for handling multiple images from url
+    init(urls: Array<String>) {
+        for (i, url) in urls.enumerated() {
+            
+            //-------------------------------------------------------------------------
+            // File retrieval
+            // Temporary implementation. Very slow blocking code added to help test anchoring
+            // Zhihao will replace with better code later
+            let remoteURL = URL(string: url)!
+            let fileURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+            let data = try! Data(contentsOf: remoteURL)
+            try! data.write(to: fileURL)
+            //-------------------------------------------------------------------------
+            
+            // Create a box mesh
+            let len = 0.2
+            let height = 0.2
+            let mesh = MeshResource.generateBox(size: [Float(len), 0.0001, Float(height)])
+            
+            // Create a material
+            var material = SimpleMaterial()
+            let texture = try! TextureResource.load(contentsOf: fileURL)
+            material.color.texture = PhysicallyBasedMaterial.Texture(texture)
+            
+            // Create a model entity with the mesh and material
+            let entity = ModelEntity(mesh: mesh, materials: [material])
+            
+            // Create an anchor and add the entity to the scene
+            let numRows = 3
+            let row = i % numRows
+            let col = Int(i / numRows)
+            let x = 0 + ((len + 0.01) * Double(col))
+            let y = -0.5
+            let z = -0.5 + ((height + 0.01) * Double(row))
+            let anchor = AnchorEntity(world: [Float(x), Float(y), Float(z)])
+            anchor.addChild(entity)
+            arView.scene.addAnchor(anchor)
+        }
     }
 }
 
